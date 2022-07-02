bfe9a9cc104be569a1fd432bd7f8efe813e565ab
franklin981 <franklinfreeman01@gmail.com>
2022-07-02 02:49:27 -0700

    ALX if else statment

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,24 +4,31 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * main - assign a random number to the variable each time it is executed
+ *and prints out based on condition
+ *Return: always 0 (success)
+ */
 int main(void)
 {
-  int n;
+  /* main program starts and the return has return of zero */
+int n;
 
-  srand(time(0));
-  n = rand() - RAND_MAX / 2;
-  /* your code goes there */
-  if (n > 0)
-  {
-    printf("%d is positive\n", n);
-  }
-  else if (n == 0)
-  {
-    printf("%d is zero\n", n);
-  }
-  else
-  {
-    printf("%d is negative\n", n);
-  }
-  return (0);
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
+if (n > 0)
+{
+printf("%d is positive\n", n);
 }
+else if (n == 0)
+{
+printf("%d is zero\n", n);
+}
+else
+{
+printf("%d is negative\n", n);
+}
+return (0);
+}
+
