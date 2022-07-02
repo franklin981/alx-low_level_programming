abbd56d05032f9ce0773b5872d90bd00b71f1f73
franklin981 <franklinfreeman01@gmail.com>
2022-07-02 06:27:18 -0700

    working with variables

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,11 +1,17 @@
 #include <stdlib.h>
 #include <stdio.h>
+/**
+ *main -> assign a random number to the variable n each time
+ *and print the last digit of the number stored in the
+ *Return: Always 0 (Success)
+ */
 int main(void)
 {
 int ch;
-for (ch = 'a'; ch <= 'z'; ch++)
+for (ch = 97; ch <= 122; ch++)
 {
-  putchar(ch);
+putchar(ch);
 }
-return 0;
+putchar(10); /* this is an ascii code for new line*/
+return (0);
 }
