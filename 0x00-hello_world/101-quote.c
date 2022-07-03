7d68ccba578ac31610cde4e0fe36efad865723bd
franklin981 <franklinfreeman01@gmail.com>
2022-07-03 09:05:47 -0700

    working with STDOUT_FILENO

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,8 @@
  * main - where the function starts
  * Return: always 0 (success)
  */
-int main(void)  
+int main(void)
 {
-  fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
