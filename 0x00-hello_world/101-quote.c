48bad6653a4e37273bb1a7a6bc133964499fdfdc
franklin981 <franklinfreeman01@gmail.com>
2022-07-03 07:22:20 -0700

    working with STDOUT_FILENO

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - where the function starts
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+write(STDOUT_FILENO, "and that piece of art is useful
+                 \" - Dora korpar, 2015-10-19\n", 59);
+return (1);
+}
