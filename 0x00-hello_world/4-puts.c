ca336cf16b6c9114bfb3408c6e76393215283a6f
franklin981 <franklinfreeman01@gmail.com>
2022-07-03 03:19:13 -0700

    commit the c code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - the main function starts here
+ *
+ * Return: of the coe is always 0
+ */
+int main(void)
+{
+  printf("with proper grammar, but the outcome is a piece of art\n");
+  return (0);
+}
