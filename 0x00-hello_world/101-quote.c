e7b9104afe52869583b19a09dc05ccce983f98cc
franklin981 <franklinfreeman01@gmail.com>
2022-07-04 20:08:32 -0700

    making a quote in c programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 /**
- * main - where the function starts
- * Return: always 0 (success)
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\"
-- Dora Korpar, 2015-10-19\n");
+write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);  
 return (1);
 }
