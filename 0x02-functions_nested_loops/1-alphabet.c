8d2f2f955b19e32d8f447a1bf384bf03c67e6eb5
franklin981 <franklinfreeman01@gmail.com>
2022-07-06 21:17:25 -0700

    printing a to z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - entry point
+ * Return: 0 if successful
+ */
+int main(void)
+{
+int j;
+for (j = 'a'; j <= 'z'; j++)
+{
+_putchar('j');
+}
+_putchar('\n');
+return (0);
+}
