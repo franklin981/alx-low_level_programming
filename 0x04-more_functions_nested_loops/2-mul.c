0012e75ff5c337f5a8d5f2f5619bdf37a8a7c215
franklin981 <franklinfreeman01@gmail.com>
2022-07-07 16:45:59 -0700

    mul comman

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - function to hold two value
+ * @a: integer para1
+ * @b: interger para 2
+ * Return: this return a success
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
