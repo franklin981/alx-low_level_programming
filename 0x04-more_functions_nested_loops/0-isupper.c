30b4c0834a3a5d53a2bad96bfe917b3f180029be
franklin981 <franklinfreeman01@gmail.com>
2022-07-07 15:37:01 -0700

    this is good

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * isupper -> this function ti check if your alphabet is 
+ *
+ * Return: is 0
+ */
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}  
+}
