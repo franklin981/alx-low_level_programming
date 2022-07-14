f6b9a288161dc30b3619f0743ff4fd74d7e77c75
franklin981 <franklinfreeman01@gmail.com>
2022-07-13 20:36:18 -0700

    reversal

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,7 +5,6 @@
  */
 void rev_string(char *s)
 {
-char tmp;
 int i, length1, length2;
 length1 = 0;
 length2 = 0;
@@ -16,7 +15,7 @@ length1++;
 length2 = length1 - 1;
 for (i = 0; i < length1 / 2; i++)
 {
-tmp = s[i];
+char tmp = s[i];
 s[i] = s[length2];
 length2 -= 1;
 }
