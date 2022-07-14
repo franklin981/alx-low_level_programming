ce0c2755a5a951df044510e0d1db95481b59e455
franklin981 <franklinfreeman01@gmail.com>
2022-07-13 20:32:31 -0700

    reversal

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,9 +9,9 @@ char tmp;
 int i, length1, length2;
 length1 = 0;
 length2 = 0;
-while (s[length] != '\0')
+while (s[length1] != '\0')
 {
-length++;
+length1++;
 }
 length2 = length1 - 1;
 for (i = 0; i < length / 2; i++)
