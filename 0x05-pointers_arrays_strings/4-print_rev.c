3abd72f1180c9da348a3f843368c9ac8307d6890
franklin981 <franklinfreeman01@gmail.com>
2022-07-13 20:06:33 -0700

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+include "main.h"
+/**
+ *print_rev -> printing a string in reverse
+ * @s: string need to be printed reverse
+ */
+void print_rev(char *s)
+{
+int n;
+n = strlen(s);
+while (s[n] != '\0')
+{
+n++;
+}
+for (i = n - 1; i >= 0; i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
+}
