ed72e5302b6bade5b2cb640ab0718471cca0993f
franklin981 <franklinfreeman01@gmail.com>
2022-07-13 17:55:33 -0700

    swapping values

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,10 +1,14 @@
 #include "main.h"
-
 /**
- * 
+ * swap_int -> given two integer swap the values they are 
+ *holding
+ * @a : parameter 1
+ * @b : parameter 2
  */
 void swap_int(int *a, int *b)
 {
-*a = b;
-*b = a;
+int tmp;
+tmp = *a;
+*a = *b;
+*b = tmp;
 }
