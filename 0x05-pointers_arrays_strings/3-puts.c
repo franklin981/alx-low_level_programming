cc39b26b5e18e3571eac0fed01bfe266cfcf82f5
franklin981 <franklinfreeman01@gmail.com>
2022-07-13 19:43:52 -0700

    putchar

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts -> using of puts function
+ * @ *str - string to be printed
+ */
+
+void _puts(char *str)
+{
+for(; *str != '\0'; str++)
+{
+_putchar(*str);
+}
+_putchar('\n');
+}
